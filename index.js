a6ced7ef4b4ca55e8bbdc833f2cdb0a64560ee6b
shaoshxuan <shaoshxuan.chua@gmail.com>
2020-05-30 18:51:20 +0800

    Added Sticky Navigation Bar
    Added transition for navigation bar to fade out on scroll down and fade in on scroll up, as well as blend in on reaching page top.

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ document.addEventListener("DOMContentLoaded", (e) => {
         document.getElementsByTagName("body")[0].style.overflowY = "visible";
     }, 7000);
 })
+
+// Navigation Bar Fade onScroll
+let scrollYOffset = 0;
+window.addEventListener("scroll", function(){
+    let header = document.querySelector("header");
+    if (window.pageYOffset > 0){
+        if (window.pageYOffset > scrollYOffset){
+            header.classList = "";
+            header.classList.add("navFadeOut");
+        } else {
+            header.classList = "";
+            header.classList.add("navFadeIn");
+        }
+        scrollYOffset = window.pageYOffset;
+    } else {
+        header.classList = "";
+    }
+})
